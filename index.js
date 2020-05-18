5065e6d9b15ed9baed369815744f0aaec8fa7ccd
Tom Barton <tom.r.barton@gmail.com>
2020-05-18 21:21:39 +0100

    Remove debugs

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ const testEndpoint = async () => {
 
     const request = await fetch(endpoint);
 
-    console.log(request);
     // Throw an error if the request is anything other than 2xx.
     if (!request.ok) {
       throw new Error("Not 2xx response.");
     }
 
     const result = await request.json();
-    console.log(result);
     core.setOutput("output", JSON.stringify(result));
   } catch (err) {
     core.setFailed(err.message);
